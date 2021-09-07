93efcdeeaef6bf5617a88119b09551d4655d9f0a
jass1302 <mijumarutv@gmail.com>
2021-09-06 22:57:46 -0500

    Cosas basicas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">JasShop</a>
+        </div>
+        <div>
+          <a href="/cart">Carrito</a>
+        </div>
+        <div>
+          <a href="/signin">Ingresar</a>
+        </div>
       </header>
+      <main>
+        <div className="row center">
+        {
+          data.products.map((product) =>(
+            <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+              <img className="medium" src={product.image} alt={product.name}/>
+            </a>
+            <div className="card-body">
+            <a href={`/product/${product._id}`}>
+                <h2>{product.name}</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">${product.price}</div>
+            </div>
+          </div>
+          ))
+        }
+        </div>
+      </main>
+      <footer className="row center">Desarrollo en progreso por: Jassiel Hernández</footer>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Playera de Pikáchu',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 149,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+        {
+            _id: '2',
+            name: 'Playera de Pichu',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 99,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+        {
+            _id: '3',
+            name: 'Playera de Raichu',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 99,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+        {
+            _id: '4',
+            name: 'Playera de Charizard',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 399,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+        {
+            _id: '5',
+            name: 'Playera de Gengar',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 190,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+        {
+            _id: '6',
+            name: 'Tazita de uWu',
+            category: 'Mugs',
+            image: '/images/p6.jpg',
+            price: 100,
+            brand: 'JassBrand',
+            rating: 4.5,
+            numReviews: 10,
+            desc: 'Diseñada por el mismísimo Satoshi Tajiri'
+        },
+    ]
+};
+
+export default data;
