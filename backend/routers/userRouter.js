7370dde75f0c8eab60e10ab0a07abcb5bb8eaa46
jass1302 <mijumarutv@gmail.com>
2021-09-08 17:54:22 -0500

    Sign In API implementada y probada

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,9 @@
 import expres from 'express';
+import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = expres.Router();
 
@@ -13,4 +15,25 @@ userRouter.get('/seed', expressAsyncHandler (async (req, res) =>{
     res.send({createdUsers});
 }));
 
+userRouter.post('/signin', expressAsyncHandler(
+    async (req, res) =>{
+        console.log(req.body);
+        const user = await User.findOne({email: req.body.email});
+        if (user) {
+            console.log("Usuario encontrado");
+            if (bcrypt.compareSync(req.body.pass, user.pass)) {
+                console.log("Pass correcta");
+                res.send({
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    isAdmin: user.isAdmin,
+                    token: generateToken(user)
+                });
+                return;
+            }
+        }
+        res.send(401).send({message: "Usuario o contraseña invalidos"});
+    }
+));
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/jasshop',{
     useNewUrlParser: true
@@ -13,6 +20,7 @@ app.use('/api/products',productRouter);
 
 app.get('/', (req, res)=>{ res.send('Server is ready!');});
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) =>{ res.status(500).send({ message: err.message});});
 
 const port = process.env.PORT || 5000;
